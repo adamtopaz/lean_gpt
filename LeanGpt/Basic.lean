4a878577dd3b7bd87ec70ec658e085d8a1f87514
adamtopaz <github@adamtopaz.com>
2023-07-03 04:39:39 -0600

    small refactor

--- LeanGpt/Basic.lean
+++ LeanGpt/Basic.lean
@@ -40,23 +40,27 @@ def getJsonResponse (req : Json) : IO Json := do
   | .ok json => return json
   | .error err => throw <| .userError s!"{err}\n{req}"
 
-def getResponse : GPTM GPT.Message := do
+def getResponses (n : Nat) : GPTM (Array GPT.Message) := do
   let req : Json := 
     Json.mkObj [
       ("model", "gpt-4"),
-      ("messages", toJson <| ← getThe (Array GPT.Message))
+      ("messages", toJson <| ← getThe (Array GPT.Message)),
+      ("n", n)
     ]
   let jsonResponse ← getJsonResponse req
   let .ok choices := jsonResponse.getObjValAs? (Array Json) "choices" | 
     throw <| .userError s!"Failed to parse choices as array:
 {jsonResponse}"
-  let some choice := choices[0]? | 
-    throw <| .userError s!"Failed to find choice:
+  let .ok choices := choices.mapM fun j => j.getObjValAs? GPT.Message "message" | 
+    throw <| .userError s!"Failed to parse messages:
 {choices}"
-  let .ok message := choice.getObjValAs? GPT.Message "message" | 
-    throw <| .userError s!"Failed to find message:
-{choice}"
-  return message
+  return choices
+
+def getResponse : GPTM GPT.Message := do
+  let msgs ← getResponses 0 
+  let some msg := msgs[0]? | 
+    throw <| .userError s!"No messages were returned." 
+  return msg
 
 def streamResponse : GPTM Unit := do
   let req : Json := Json.mkObj [
